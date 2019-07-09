ee75a5456c9837192db5acc84f52655140ccc0cf
yHan <i_boxes@163.com>
2019-07-09 16:49:55 +0800

    121

--- CCTest/TestWeb/Properties/AssemblyInfo.cs
+++ CCTest/TestWeb/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("fcd95a01-dccd-4624-8c77-ffbf43bb5c12")]
+[assembly: Guid("3721ccb7-c1b1-4756-8349-54c6656149db")]
 
 // 程序集的版本信息由下列四个值组成:
 //
